efbab58b6c1acc5281575e28100f3100de2c1fbf
Matthias Hennemeyer <mhennemeyer@gmail.com>
2020-07-07 16:23:10 +0200

    CloudKit - Image Asset

---
+++ Sources/swift-toolbox/CloudKit/ImageAsset.swift
@@ -0,0 +1,53 @@
+import CloudKit
+import UIKit
+
+class ImageAsset {
+
+    let image: UIImage
+
+    var url: URL?
+
+    var asset:CKAsset? {
+        get {
+            let data = self.image.pngData()
+            self.url = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(NSUUID().uuidString+".dat")
+            if let url = self.url {
+                do {
+                    try data!.write(to: url)
+                    print("write foto to disk")
+                } catch {
+                    print(error)
+                }
+                return CKAsset(fileURL: url)
+            }
+            print("error creating asset from image")
+            return nil
+        }
+    }
+
+    init(image:UIImage){
+        self.image = image
+    }
+
+    func clear() {
+        print("clear image asset")
+        if let url = self.url {
+            do {
+                try FileManager.default.removeItem(at: url) }
+            catch let e {
+                print("Error deleting temp file: \(e)")
+            }
+        }
+    }
+
+
+}
+
+extension CKAsset {
+    func uiImage() -> UIImage? {
+        if let data = try? Data(contentsOf: (fileURL!)), let image = UIImage(data: data) {
+            return image
+        }
+        return nil
+    }
+}
