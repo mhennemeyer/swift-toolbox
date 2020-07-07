6fb2633dead6000554f152e474c6f69d47ffeab6
Matthias Hennemeyer <mhennemeyer@gmail.com>
2020-07-07 16:18:46 +0200

    ImageZoomView

---
+++ Sources/swift-toolbox/UIKit/ImageZoomView/ImageZoomView.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+class ImageZoomView: UIScrollView, UIScrollViewDelegate {
+    var imageView: UIImageView!
+    var gestureRecognizer: UITapGestureRecognizer!
+    
+    convenience init(frame: CGRect, image: UIImage, minimumZoomScale: CGFloat = 1, maximumZoomScale: CGFloat = 2) {
+        self.init(frame: frame)
+
+        imageView = UIImageView(image: image)
+        imageView.frame = frame
+        imageView.contentMode = .scaleAspectFit
+        addSubview(imageView)
+
+        
+        self.minimumZoomScale = minimumZoomScale
+        self.maximumZoomScale = maximumZoomScale
+        delegate = self
+        
+        setupGestureRecognizer()
+    }
+    
+    func setupGestureRecognizer() {
+        gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap))
+        gestureRecognizer.numberOfTapsRequired = 2
+        addGestureRecognizer(gestureRecognizer)
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    @objc func handleDoubleTap() {
+        if zoomScale == 1 {
+            zoom(to: zoomRectForScale(maximumZoomScale, center: gestureRecognizer.location(in: gestureRecognizer.view)), animated: true)
+        } else {
+            setZoomScale(1, animated: true)
+        }
+    }
+    
+    func zoomRectForScale(_ scale: CGFloat, center: CGPoint) -> CGRect {
+        var zoomRect = CGRect.zero
+        zoomRect.size.height = imageView.frame.size.height / scale
+        zoomRect.size.width = imageView.frame.size.width / scale
+        let newCenter = convert(center, from: imageView)
+        zoomRect.origin.x = newCenter.x - (zoomRect.size.width / 2.0)
+        zoomRect.origin.y = newCenter.y - (zoomRect.size.height / 2.0)
+        return zoomRect
+    }
+}
+
