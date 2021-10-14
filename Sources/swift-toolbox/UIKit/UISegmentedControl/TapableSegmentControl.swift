2f2d3c09ec4cd0f60a03a28dc88cf1f9f970a180
Matthias Hennemeyer <mhennemeyer@gmail.com>
2021-10-14 09:13:06 +0200

    onTapOnSelected

--- Sources/swift-toolbox/UIKit/UISegmentedControl/TapableSegmentControl.swift
+++ Sources/swift-toolbox/UIKit/UISegmentedControl/TapableSegmentControl.swift
@@ -8,13 +8,16 @@
 import UIKit
 
 public class TapableSegmentControl : UISegmentedControl {
-    var onTapSelected: (Int -> Void);
+    var onTapSelected: ((Int) -> Void) = {_ in };
     
     public override var selectedSegmentIndex: Int {
         get {
             super.selectedSegmentIndex
         }
         set {
+            if selectedSegmentIndex == newValue {
+                onTapSelected(selectedSegmentIndex)
+            }
             super.selectedSegmentIndex = newValue
         }
     }
