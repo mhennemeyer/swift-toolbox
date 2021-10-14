c7e459751dc60159de6c9f7c88f3512b176eb8ed
Matthias Hennemeyer <mhennemeyer@gmail.com>
2021-10-14 09:44:46 +0200

    TapableSegment Without override

--- Sources/swift-toolbox/UIKit/UISegmentedControl/TapableSegmentControl.swift
+++ Sources/swift-toolbox/UIKit/UISegmentedControl/TapableSegmentControl.swift
@@ -10,20 +10,9 @@ import UIKit
 public class TapableSegmentControl : UISegmentedControl {
     public var onTap: ((Int, Int) -> Void) = {_, _ in };
     
-    var priorSelected: Int = 0
-    
-    public override var selectedSegmentIndex: NSInteger {
-        get {
-            super.selectedSegmentIndex
-        }
-        set {
-            onTap(selectedSegmentIndex, newValue)
-            super.selectedSegmentIndex = newValue
-        }
-    }
-    
     public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        //onTapSelected(oldValue)
+        let oldValue = selectedSegmentIndex
         super.touchesBegan(touches, with: event)
+        onTap(oldValue, selectedSegmentIndex)
     }
 }
