154a112fd2f85405136ad803ad23021e6d9e8d41
Matthias Hennemeyer <mhennemeyer@gmail.com>
2021-10-14 09:15:45 +0200

    publish onTap

--- Sources/swift-toolbox/UIKit/UISegmentedControl/TapableSegmentControl.swift
+++ Sources/swift-toolbox/UIKit/UISegmentedControl/TapableSegmentControl.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 public class TapableSegmentControl : UISegmentedControl {
-    var onTapSelected: ((Int) -> Void) = {_ in };
+    public var onTapSelected: ((Int) -> Void) = {_ in };
     
     public override var selectedSegmentIndex: Int {
         get {
