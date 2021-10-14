9b62a05f58547e1e80fe02df53089890a5c81394
Matthias Hennemeyer <mhennemeyer@gmail.com>
2021-10-14 09:47:01 +0200

    touches ended, not began

--- Sources/swift-toolbox/UIKit/UISegmentedControl/TapableSegmentControl.swift
+++ Sources/swift-toolbox/UIKit/UISegmentedControl/TapableSegmentControl.swift
@@ -10,7 +10,7 @@ import UIKit
 public class TapableSegmentControl : UISegmentedControl {
     public var onTap: ((Int, Int) -> Void) = {_, _ in };
     
-    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         let oldValue = selectedSegmentIndex
         super.touchesBegan(touches, with: event)
         onTap(oldValue, selectedSegmentIndex)
